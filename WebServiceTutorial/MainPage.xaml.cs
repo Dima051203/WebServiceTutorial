d52fed29eeed1114aca6638ad5a0e5293d1b446f
Дмитрий Севрюков <sevryukov.d2003@gmail.com>
2021-12-08 20:27:07 +0300

    В MainPage.xaml.cs заменил код

--- WebServiceTutorial/MainPage.xaml.cs
+++ WebServiceTutorial/MainPage.xaml.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace WebServiceTutorial
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        RestService _restService;
+
         public MainPage()
         {
             InitializeComponent();
+            _restService = new RestService();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            List<Repository> repositories = await _restService.GetRepositoriesAsync(Constants.GitHubReposEndpoint);
+            collectionView.ItemsSource = repositories;
         }
     }
 }
